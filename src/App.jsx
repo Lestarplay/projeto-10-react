241155f7f68ba68a9dcf4ea4c7c8d8f1680d26c6
Lestarplay <162745949+Lestarplay@users.noreply.github.com>
2024-12-10 20:09:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react' 
+import Quiz from './components/Quiz'
+
+function App () {
+  return (
+    <div className="App">
+       <Quiz />
+    </div>
+  )
+}
+export default App
